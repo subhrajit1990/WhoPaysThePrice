b964492294fb72e022f4f8eeded7944e9245b381
subhrajit1990 <subhrajitsahu1990@gmail.com>
2023-07-20 09:33:54 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.483908115fa671e4a99d6015dffdcfb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e20be4cbda3403fe92c2",
+    "url": "/whopaystheprice/static/css/main.abf2a20b.chunk.css"
+  },
+  {
+    "revision": "e20be4cbda3403fe92c2",
+    "url": "/whopaystheprice/static/js/main.e20be4cb.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/whopaystheprice/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "f7eb488aa0d2c78eada4",
+    "url": "/whopaystheprice/static/css/2.2922c66b.chunk.css"
+  },
+  {
+    "revision": "f7eb488aa0d2c78eada4",
+    "url": "/whopaystheprice/static/js/2.f7eb488a.chunk.js"
+  },
+  {
+    "revision": "230f440d00eb5cb71f6e",
+    "url": "/whopaystheprice/static/js/runtime~main.230f440d.js"
+  },
+  {
+    "revision": "d3671c72fa20c89566bcb39546c131eb",
+    "url": "/whopaystheprice/index.html"
+  }
+];
